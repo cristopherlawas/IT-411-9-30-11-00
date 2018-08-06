d4e390432f291a66614cb53785bfb081e6ad8416
wifi user1 <login1@cec.edu.ph>
2018-08-06 11:01:17 +0800

    CRUD Process

---
+++ resources/views/crushes/index.blade.php
@@ -0,0 +1,30 @@
+<table border="1">
+	<thead>
+		<tr>
+			<td>Firstname</td>
+			<td>Lastname</td>
+			<td>FB Profile</td>
+			<td>Contact Number</td>
+			<td>Action</td>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($crushes as $crush)
+		<tr>
+			<td>{{ $crush->first_name        }}</td>
+			<td>{{ $crush->last_name         }}</td>
+			<td>{{ $crush->fb_profile_link   }}</td>
+			<td>{{ $crush->contact_number    }}</td>
+			<td><a href="{{ route('crushes.id.edit', array('id'=>$crush->id)) }}">Edit</a></td>
+			<td><a href="{{ route('crushes.id.destroy', array('id'=>$crush->id)) }}">Delete</a></td>
+		</tr>
+		@endforeach
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td><a href="{{ route('crushes.create')}}">Add New Crush</a></td>
+		</tr>
+	</tbody>
+</table>
